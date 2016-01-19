7dc03bdf5b2c8d0b51bd772c8e98e533fbcf9e8b
Scott Munro <scottmunro@utexas.edu>
2016-01-19 09:45:49 -0600

    wrote the create and use databases routines which consequently required me to define the constructor of the Database object which will take in a name

--- pyp_database/db/db.py
+++ pyp_database/db/db.py
@@ -1,8 +1,5 @@
 import pyp_database.table as table
 
 class Database:
-    def create_table(self, name, columns):
-        return table.Table([])
-
-# test code to make sure imports are working
-testtable = table.Table([])
+    def __init__(self, name):
+        self.name = name
--- pyp_database/db_manager.py
+++ pyp_database/db_manager.py
@@ -2,11 +2,19 @@ import pyp_database.db as db
 import os
 import pickle
 
+databases = {}
+
 def create_database(name):
-    pass
+    if name in databases:
+        err = "Name for new database <{}> already in use".format(name)
+        raise ValueError(err)
+    databases[name] = db.Database(name)
 
 def use(name):
-    return db.Database()
+    if name not in databases:
+        err = "No database currently loaded with the name <{}>".format(name)
+        raise ValueError(err)
+    return databases[name]
 
 def save_database(db_name, file_name):
     pass
