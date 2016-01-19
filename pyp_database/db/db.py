66c0736ee95c4a88c95670c5fd66f4ae6d12acec
Scott Munro <scottmunro@utexas.edu>
2016-01-19 15:03:21 -0600

    forgot to update the db with the intermittent commit

--- pyp_database/db/db.py
+++ pyp_database/db/db.py
@@ -3,3 +3,6 @@ import pyp_database.table as table
 class Database:
     def __init__(self, name):
         self.name = name
+
+    def create_table(self, name, columns):
+        setattr(self, name, table.Table(columns))
