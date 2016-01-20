3f801f090b9d8bba79472d0607ca3def4c22c71b
Scott Munro <scottmunro@utexas.edu>
2016-01-19 23:26:30 -0600

    added the inserting functionality into the database

--- example.py
+++ example.py
@@ -5,5 +5,5 @@ pyp_database.create_database('imdb')
 db = pyp_database.use('imdb')
 
 db.create_table('actors', columns=['id', 'name', 'dob'])
-# db.actors.insert(1, 'Kevin Bacon' date(1958, 7, 8))
+db.actors.insert(1, 'Kevin Bacon', date(1958, 7, 8))
 # db.actors.query(name='Kevin Bacon')
--- pyp_database/table/table.py
+++ pyp_database/table/table.py
@@ -1,9 +1,23 @@
 class Table:
     def __init__(self, columns):
-        pass
+        self.columns = columns
+        self.map = {col: {} for col in columns}
+
+    def __key_exists(self, column_id, key):
+        return key in self.map[self.columns[column_id]]
+
+    def __insert_new_key(self, column_id, key, val):
+        self.map[self.columns[column_id]][key] = [val]
+
+    def __append_to_key(self, column_id, key, val):
+        self.map[self.columns[column_id]][key].append(val)
 
     def insert(self, *args):
-        pass
+        for i in range(len(args)):
+            if self.__key_exists(i, args[i]):
+                self.__append_to_key(i, args[i], args)
+            else:
+                self.__insert_new_key(i, args[i], args)
 
     def query(self, **kwargs):
         pass
