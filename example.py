30fa90ab55a103ef6a31e631c7ef8677a196d096
Scott Munro <scottmunro@utexas.edu>
2016-01-19 23:45:45 -0600

    wrote the query function

--- example.py
+++ example.py
@@ -6,4 +6,7 @@ db = pyp_database.use('imdb')
 
 db.create_table('actors', columns=['id', 'name', 'dob'])
 db.actors.insert(1, 'Kevin Bacon', date(1958, 7, 8))
-# db.actors.query(name='Kevin Bacon')
+print db.actors.query(name='Kevin Bacon')
+db.actors.insert(2, 'Kevin Bacon', date(1995, 5, 5))
+print db.actors.query(name='Kevin Bacon')
+print db.actors.query(name='Kevin Bacon', id=1)
--- pyp_database/table/table.py
+++ pyp_database/table/table.py
@@ -12,6 +12,9 @@ class Table:
     def __append_to_key(self, column_id, key, val):
         self.map[self.columns[column_id]][key].append(val)
 
+    def __get(self, col_name, col_val):
+        return self.map[col_name][col_val]
+
     def insert(self, *args):
         for i in range(len(args)):
             if self.__key_exists(i, args[i]):
@@ -20,4 +23,16 @@ class Table:
                 self.__insert_new_key(i, args[i], args)
 
     def query(self, **kwargs):
-        pass
+        column_ids = [self.columns.index(key) for key in kwargs.keys()[1:]]
+        init_col_name, init_col_val = kwargs.items()[0]
+        qry = self.__get(init_col_name, init_col_val)
+
+        # filter out the query based on the rest of the kwargs
+        filter_kwargs = kwargs.items()[1:]
+        for col_name, col_val in filter_kwargs:
+            col_id = self.columns.index(col_name)
+            qry = [e for e in qry if e[col_id] == col_val]
+        return qry
+
+
+        
