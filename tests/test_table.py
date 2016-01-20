bd27be9dacd132a6d1ff7536d287b57f99aacf87
Scott Munro <scottnmunro@gmail.com>
2016-01-20 13:55:32 -0600

    wrote table test

---
+++ tests/test_table.py
@@ -0,0 +1,11 @@
+import unittest
+import pyp_database.table
+
+class TestDatabaseTables(unittest.TestCase):
+    def test_insert_and_query(self):
+        test_table = pyp_database.table.Table(['id', 'name', 'dob'])
+        test_table.insert(1, 'Kevin Bacon', date(1958, 7, 8))
+        self.assertEquals(len(test_table.query(name='Kevin Bacon')), 1)
+        test_table.insert(2, 'Kevin Bacon', date(1995, 5, 5))
+        self.assertEquals(len(test_table.query(name='Kevin Bacon')), 2)
+        self.assertEquals(len(test_table.query(name='Kevin Bacon', id=1)), 1)
